5ad06389efa1583b0b6d60454e92b1b5fb05d2e1
Ahmadgaber3 <ans354093@gmail.com>
2026-09-09 20:15:43 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,39 @@
-const CACHE_NAME = 'subscribers-v23';
-const ASSETS = [
-  'index.html',
-  'manifest.json'
+const CACHE_NAME = 'storia-v30';
+const STATIC_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './images/logooo.png'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) return cachedResponse;
+      return fetch(event.request).then((networkResponse) => {
+        return networkResponse;
+      }).catch(() => caches.match('./index.html'));
     })
   );
 });
